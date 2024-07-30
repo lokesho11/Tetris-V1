7e4c461e1f61277b9366ed9d43dd63fc274d80f6
lokesho11 <lokeshpatil1718@gmail.com>
2024-07-30 16:32:18 +0530

    new update

--- Jenkins-terraform/Main.tf
+++ Jenkins-terraform/Main.tf
@@ -59,7 +59,7 @@ resource "aws_security_group" "Jenkins-sg" {
 }
 
 resource "aws_instance" "web" {
-  ami                    = "ami-0df4b2961410d4cff"
+  ami                    = "ami-0ad21ae1d0696ad58"
   instance_type          = "t2.medium"
   key_name               = "key.pem"
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
